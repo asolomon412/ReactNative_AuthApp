662d101ce70f8d62c3dd369ac7ebcf45a05c1a63
Antonella Solomon <antonellasolomon@gmail.com>
2017-01-28 14:27:22 -0500

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component{
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyCUCG65FTRq4ku3BisNjJRSS6fCLaeukqk',
+    authDomain: 'authentication-28002.firebaseapp.com',
+    databaseURL: 'https://authentication-28002.firebaseio.com',
+    storageBucket: 'authentication-28002.appspot.com',
+    messagingSenderId: '573832545785'
+    });
+  }
+
   render() {
     return (
     <View>
